069be9fdad1d850dc79d0e9ec77dc289f5141de3
exlawyer <i@eaksenov.ru>
2023-03-07 14:35:52 +0300

    add step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import gendiff from '../src/index.js';
 
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('-v, --vers', 'output the current version')
-  .option('-h, --help', 'display help for command')
-  ;
-program.parse();     
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filePath1> <filePath2>')
+  .action((filePath1, filePath2) => {
+    console.log(gendiff(filePath1, filePath2));
+  });
+program.parse();
---
+++ src/index.js
@@ -0,0 +1,71 @@
+import { readFileSync } from 'node:fs';
+import _ from 'lodash';
+import { cwd } from 'node:process';
+import path from 'node:path';
+
+const genDiff = (obj1, obj2) => {
+  const allEntries = [...Object.entries(obj1), ...Object.entries(obj2)];
+  const nodes = [];
+  allEntries
+    .map((entry) => {
+      const [key, value] = entry;
+      const node = {};
+      if (!obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
+        node.key = [key];
+        node.value = [value];
+        node.status = 'added';
+      } else if (obj1.hasOwnProperty(key) && !obj2.hasOwnProperty(key)) {
+        node.key = [key];
+        node.value = [value];
+        node.status = 'deleted';
+      } else if (obj1[key] !== obj2[key]) {
+        node.key = [key];
+        node.value = [value];
+        node.status = 'changed';
+      } else {
+        node.key = [key];
+        node.value = [value];
+        node.status = 'unchanged';
+      }
+      nodes.push(node);
+    });
+  const sortedNodes = _.sortBy(nodes, 'key');
+  let result = '';
+  sortedNodes.map((node) => {
+    switch (node.status) {
+      case 'deleted':
+        result += `\n - ${node.key}: ${node.value}`;
+        break;
+      case 'unchanged':
+        if (!result.includes(node.key)) {
+          result += `\n   ${node.key}: ${node.value}`;
+          break;
+        } else {
+          return '';
+        }
+      case 'changed':
+        if (!result.includes(node.key)) {
+          result += `\n - ${node.key}: ${node.value}`;
+        } else {
+          result += `\n + ${node.key}: ${node.value}`;
+        }
+        break;
+      case 'added':
+        result += `\n + ${node.key}: ${node.value}`;
+        break;
+      default: throw new Error(`Unknown status: '${node.key}'!`);
+    }
+  });
+  return `{${result}\n}`;
+};
+
+const getPath = (filepath) => path.resolve(cwd(), filepath);
+
+export default (filePath1, filePath2) => {
+  const getFileContent1 = readFileSync(getPath(filePath1), 'utf8');
+  const getFileContent2 = readFileSync(getPath(filePath2), 'utf8');
+  const parsedFile1 = JSON.parse(getFileContent1);
+  const parsedFile2 = JSON.parse(getFileContent2);
+
+  return genDiff(parsedFile1, parsedFile2);
+};
