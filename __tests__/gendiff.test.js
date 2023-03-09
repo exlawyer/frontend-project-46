0b1b4f4f51afb2aaf359ea1c43ed1e5d1555545f
exlawyer <i@eaksenov.ru>
2023-03-09 14:03:53 +0300

    Add first test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,21 @@
 import { expect, jest, test } from '@jest/globals';
 import { readFileSync } from 'node:fs';
-// import { fileURLToPath } from 'url';
-import { resolve, dirname, path } from 'node:path';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+import { dirname } from 'path';
 // import { cwd } from 'node:process';
 
 import gendiff from '../src/index.js';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
-// const getFixturesPath = (filepath) => resolve(cwd(), '__fixtures__', filepath);
 
 test('gendiff', () => {
-  const file1Path = './__fixtures__/file1.json';
-  const file2Path = './__fixtures__/file2.json';
+  const file1Path = getFixturePath('file1.json');
+  const file2Path = getFixturePath('file2.json');
   const result = readFile('testFixtureJson.txt');
   expect(gendiff(file1Path, file2Path)).toBe(result);
 });
