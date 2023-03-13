d6cce276b08f613e3a7d8fbf9f49895d4b1a9ddd
exlawyer <i@eaksenov.ru>
2023-03-13 13:53:39 +0300

    add second test (fails)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,21 +1,25 @@
-import { expect, jest, test } from '@jest/globals';
-import { readFileSync } from 'node:fs';
+import { describe, expect, test } from '@jest/globals';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
-import path from 'node:path';
-import { dirname } from 'path';
+import path from 'path';
+// import { dirname } from 'path';
 // import { cwd } from 'node:process';
 
 import gendiff from '../src/index.js';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff', () => {
-  const file1Path = getFixturePath('file1.json');
-  const file2Path = getFixturePath('file2.json');
-  const result = readFile('testFixtureJson.txt');
-  expect(gendiff(file1Path, file2Path)).toBe(result);
+const formats = ['json', 'yaml', 'yml'];
+
+describe('test gendiff', () => {
+  test.each(formats)('gendiff with %s format', (format) => {
+    const file1Path = getFixturePath(`file1.${format}`);
+    const file2Path = getFixturePath(`file2.${format}`);
+    const result = readFile(`testFixture-${format}.txt`);
+    expect(gendiff(file1Path, file2Path)).toBe(result);
+  });
 });
